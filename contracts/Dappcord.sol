5fb64748a766d207135657171a68af278c72c6d4
Scott Peat <scottpeatweb@gmail.com>
2023-03-20 22:28:24 +0000

    Working on Solidity Files and testing with Mocha

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
+    uint256 public totalSupply;
     uint256 public totalChannels;
     address public owner;
 
@@ -15,15 +16,25 @@ contract Dappcord is ERC721 {
 
     mapping(uint256 => Channel) public channels;
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     constructor(string memory _name, string memory _symbol) 
     ERC721(_name, _symbol) 
     {
         owner = msg.sender;
     }
 
-    function createChannel(string memory _name, uint256 _cost) public {
+    function createChannel(string memory _name, uint256 _cost) public onlyOwner {   
         totalChannels++;
         channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function mint (uint256 _id) public payable {
+        
+        _safeMint(msg.sender, 1);
     }
 
     function getChannel(uint256 _id) public view returns (Channel memory) {
