9ce14fe0ccfdcd7ddb62767c845b2f1a3692f003
Scott Peat <scottpeatweb@gmail.com>
2023-03-22 08:35:50 +0000

    Completed developing solidity smart contract and tested with Chai and now working on deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,21 @@
-const hre = require("hardhat")
+const hre = require('hardhat');
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+};
 
 async function main() {
+  // Set up accounts & variables
+  const [deployer] = await ethers.getSigners();
+  const NAME = 'Dappcord';
+  const SYMBOL = 'DC';
 
+  // Deploy contract
+  const Dappcord = await ethers.getContractFactory('Dappcord');
+  const dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  await dappcord.deployed();
+
+  console.log(`Deployed Dappcord Contract at: ${dappcord.address}\n`);
 }
 
 main().catch((error) => {
